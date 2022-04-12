b1c01d1e6ddb113899372bd320e507587d8b98a5
Kristiyan Strahilov <kdstrahilov@gmail.com>
2022-04-12 18:55:28 +0300

    set crossorigin to localhost:3000 and allowed credentials
    - gitignored .vscode folder

--- src/main/java/com/bezkoder/spring/login/controllers/AuthController.java
+++ src/main/java/com/bezkoder/spring/login/controllers/AuthController.java
@@ -34,7 +34,7 @@ import com.bezkoder.spring.login.repository.UserRepository;
 import com.bezkoder.spring.login.security.jwt.JwtUtils;
 import com.bezkoder.spring.login.security.services.UserDetailsImpl;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
