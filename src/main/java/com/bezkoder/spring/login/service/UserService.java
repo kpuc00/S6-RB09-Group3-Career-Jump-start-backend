89c318c8dfd2cecb7dea618fcd7ad8ccccb138fb
Sava Hristov <i400752@fhict.nl>
2022-06-02 18:33:36 +0200

    username endpoint

--- src/main/java/com/bezkoder/spring/login/service/UserService.java
+++ src/main/java/com/bezkoder/spring/login/service/UserService.java
@@ -24,7 +24,7 @@ public interface UserService  {
 
     User updateUserStatus(Long id, User user);
 
-    User updateUserQuestionnaire(Long id);
+    User updateUserQuestionnaire(String username);
 
     void deleteUser(Long id);
 }
