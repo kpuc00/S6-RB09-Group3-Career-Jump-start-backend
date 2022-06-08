9ff1aabaa1dbfeda037b097277fc503e11c003cf
Georgi2704 <jrmanev@gmail.com>
2022-06-08 19:28:06 +0200

    for load testing

--- src/main/java/com/bezkoder/spring/login/Config.java
+++ src/main/java/com/bezkoder/spring/login/Config.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,18 +28,32 @@ public class Config {
     private String directXchangeName;
 
     @Bean
+    @Qualifier("queue")
     public Queue queue() {
         return new Queue(queueName);
     }
 
     @Bean
+    @Qualifier("queueTest")
+    public Queue queueTest() {
+        return new Queue("queueTest");
+    }
+
+    @Bean
+    @Qualifier("directExchange")
     public DirectExchange directExchange() {
         return new DirectExchange(directXchangeName);
     }
 
+
     @Bean
-    public Binding binding(DirectExchange exchange, Queue queue) {
+    public Binding binding(@Qualifier("directExchange") DirectExchange exchange, @Qualifier("queue")Queue queue) {
         return BindingBuilder.bind(queue).to(exchange).with("roytuts");
+    }
+
+    @Bean
+    public Binding bindingTest(@Qualifier("directExchange") DirectExchange exchangeTest, @Qualifier("queueTest")Queue queueTest) {
+        return BindingBuilder.bind(queueTest).to(exchangeTest).with("test");
     }
 
     @Bean
